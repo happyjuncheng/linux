080b72d7a3c8f2379e7b86ca06dabf6c65f67c53
zhujuncheng <83916586@qq.com>
2018-11-18 20:57:21 +0800

    -

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,6 @@
 # 统计有多少种架构的rpm包
 ls *.rpm | rev | cut -d. -f2 | rev | sort | uniq -c
 ls | sed -r 's/.*\.(.*)\.rpm$/\1/' | sort | uniq -c
+
+# 统计linux有多少行代码
+find linux-4.18.13/ -name '*.c' -exec cat {} \; | wc -l
